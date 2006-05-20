457b613681f582d6fc97401fd5645ea21d90b5db
Andreas Kloeckner <mathem@tiker.net>
2006-05-20 06:24:55 +0000

    [konnekte @ Arch-1:inform@tiker.net--2006-public%konnekte--mainline--0--patch-4] Rapid growth

--- client/upload.sh
+++ client/upload.sh
@@ -5,6 +5,5 @@ ssh root@$DESTMACHINE "rm -R konnekte"
 ssh root@$DESTMACHINE "mkdir -p konnekte/bin"
 ssh root@$DESTMACHINE "mkdir -p konnekte/etc"
 ssh root@$DESTMACHINE "mkdir -p konnekte/var"
-scp -r ../router/var/* root@$DESTMACHINE:konnekte/var
-scp -r ../router/etc/* root@$DESTMACHINE:konnekte/etc
-scp -r ../router/bin/* root@$DESTMACHINE:konnekte/bin
+scp -r ../router/bin/konnekte root@$DESTMACHINE:konnekte/bin
+scp -r ../router/etc/*[a-zA-Z] root@$DESTMACHINE:konnekte/etc
